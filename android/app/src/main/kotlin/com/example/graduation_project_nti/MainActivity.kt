8097d2c122b0ed9f4993ffde55f8affab778e3e9
yousef1719 <yousef.magdy.1119@gmail.com>
2025-12-20 23:10:31 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/graduation_project_nti/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.graduation_project_nti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GraduationProjectNti());
+}
+
+class GraduationProjectNti extends StatelessWidget {
+  const GraduationProjectNti({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
