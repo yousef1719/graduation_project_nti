d9fcfed1ba871b3b980653df138cf22926701e43
Mohamed Hassan <itsmohassan@gmail.com>
2025-12-21 19:57:37 +0200

    Add register screen UI and auth assets

---
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -0,0 +1,252 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:graduation_project_nti/features/auth/presentation/widgets/custom_TextFormField.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool isActive = false;
+  bool? isCheckBoxActive = false;
+
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    fullNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back_ios_new_outlined,
+            color: Color(0XFF171717),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Create Account',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  'Sign up to discover exclusive accessories.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xff737373),
+                  ),
+                ),
+                SizedBox(height: 30),
+                CustomTextField(
+                  labelText: "Full Name",
+                  hintText: "Enter your full name",
+                  controller: fullNameController,
+                ),
+                SizedBox(height: 10),
+                CustomTextField(
+                  labelText: "Email Address",
+                  hintText: "name@example.com",
+                  controller: emailController,
+                ),
+                SizedBox(height: 10),
+                CustomTextField(
+                  labelText: "Password",
+                  hintText: "Create a password",
+                  obscureText: isActive,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isActive = !isActive;
+                      });
+                    },
+                    icon: Icon(
+                      isActive
+                          ? Icons.visibility_off_outlined
+                          : Icons.visibility_outlined,
+                    ),
+                    color: Color(0xff9A594C),
+                    iconSize: 20,
+                  ),
+                  controller: passwordController,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                      value: isCheckBoxActive,
+                      checkColor: Colors.white,
+                      activeColor: Color(0xffEC3713),
+                      onChanged: (newValue) {
+                        setState(() {
+                          isCheckBoxActive = newValue;
+                        });
+                        log(isCheckBoxActive.toString());
+                      },
+                    ),
+                    Expanded(
+                      child: Text(
+                        'I agree to the Terms & Conditions and Privacy Policy',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff525252),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffEC3713),
+                    minimumSize: Size(double.infinity, 56),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(child: Divider(thickness: 1)),
+                    Text(
+                      '  Or sign up with  ',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xff737373),
+                      ),
+                    ),
+                    Expanded(child: Divider(thickness: 1)),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton.icon(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(color: Colors.grey),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 12),
+                          minimumSize: Size(double.infinity, 48),
+                        ),
+                        icon: SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: SvgPicture.asset('assets/icons/google.svg'),
+                        ),
+                        label: Text(
+                          'Google',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: OutlinedButton.icon(
+                        onPressed: () {},
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(color: Colors.grey),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 12),
+                          minimumSize: Size(double.infinity, 48),
+                        ),
+                        icon: SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: SvgPicture.asset('assets/icons/facebook.svg'),
+                        ),
+                        label: Text(
+                          'Facebook',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xff525252),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0XFFEC3713),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
